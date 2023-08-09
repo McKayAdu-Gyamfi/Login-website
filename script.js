970975491f437ba30183c431518572a16acb803d
Nana Kwaku <nanadaasebrekwaku@gamil.com>
2023-08-09 20:51:39 +0000

    made it responsive

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+
+const menuIcon = document.querySelector('.menu-icon')
+const navigation = document.querySelector('.navigation')
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+
+menuIcon.addEventListener('click', ()=>{
+    navigation.classList.toggle('mobile-menu')
+});
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
